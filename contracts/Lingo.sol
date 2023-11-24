e961dad656d2898c14e230fccb918394af424502
justin-johns-acc <justin.johns@accubits.com>
2023-11-24 20:11:38 +0530

    Revert "fix: C01. Total Supply Violated"
    This reverts commit dfc467f5baeab342a2f9c64309896b050fba3705.

--- contracts/Lingo.sol
+++ contracts/Lingo.sol
@@ -15,9 +15,6 @@ contract LINGO is ERC20Burnable, Ownable {
 
   // Constants
 
-  // The max supply of token ever available in circulation
-  uint256 private constant MAX_SUPPLY = 1_000_000_000 * (10 ** 18);
-
   // Representing 5% as 500
   uint256 private constant FIVE_PERCENT = 500;
 
@@ -61,7 +58,7 @@ contract LINGO is ERC20Burnable, Ownable {
    * @dev Constructor function to initialize values when the contract is created.
    * @param name_ A string representing the name of the token.
    * @param symbol_ A string representing the symbol of the token.
-   * @param initialSupply An unsigned integer representing the initial total supply of tokens for the contract.
+   * @param totalSupply_ An unsigned integer representing the initial total supply of tokens for the contract.
    * @param owner_ An address representing the owner of the contract.
    * @param treasuryAddress_ An address representing the treasury wallet address.
    * @param txnFee_ An unsigned integer representing the percentage transfer fee associated with each token transfer.
@@ -69,7 +66,7 @@ contract LINGO is ERC20Burnable, Ownable {
   constructor(
     string memory name_,
     string memory symbol_,
-    uint256 initialSupply,
+    uint256 totalSupply_,
     address owner_,
     address treasuryAddress_,
     uint256 txnFee_
@@ -88,7 +85,7 @@ contract LINGO is ERC20Burnable, Ownable {
      * which can be specified by adding a 'decimals' variable to the contract.
      * Finally, the tokens are minted and assigned to the contract owner's address.
      */
-    uint256 intialTokenSupply = initialSupply * (10 ** decimals());
+    uint256 intialTokenSupply = totalSupply_ * (10 ** decimals());
     _mint(owner_, intialTokenSupply);
 
     /**
@@ -139,12 +136,11 @@ contract LINGO is ERC20Burnable, Ownable {
   }
 
   /**
-   * @dev Can mint new tokens upto the max supply limit.
+   * @dev Mint new tokens.
    * @param to The address to mint the tokens to.
    * @param amount The amount of tokens to mint.
    */
   function mint(address to, uint256 amount) external onlyOwner {
-    require(totalSupply() + amount <= MAX_SUPPLY, "LINGO: cap exceeded");
     _mint(to, amount);
   }
 
